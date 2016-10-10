93c7d521dd92e1824e0fb5a6764e0895a75baa8b
UltimateRex <ultimaterex.rexus@gmail.com>
2016-10-10 18:21:04 -0300

    Music
    Added some sweet tunes. also changed sprite to monstercat

--- core/src/com/rexserubii/solacedhistoria/GDXgame.java
+++ core/src/com/rexserubii/solacedhistoria/GDXgame.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
