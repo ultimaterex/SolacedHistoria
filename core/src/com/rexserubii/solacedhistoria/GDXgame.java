9127fd7e0653f9220cafc07aab011576c74c69dc
UltimateRex <ultimaterex.rexus@gmail.com>
2016-10-10 22:34:33 -0300

    Removed Input Processor
    Removed Input Processor

--- core/src/com/rexserubii/solacedhistoria/GDXgame.java
+++ core/src/com/rexserubii/solacedhistoria/GDXgame.java
@@ -3,7 +3,6 @@ package com.rexserubii.solacedhistoria;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,7 +10,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-public class GDXgame extends ApplicationAdapter implements InputProcessor{
+public class GDXgame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
     Sprite sprite;
@@ -23,6 +22,7 @@ public class GDXgame extends ApplicationAdapter implements InputProcessor{
 		img = new Texture("MCat.jpg");
 		sprite = new Sprite(img);
         sprite.setPosition(Gdx.graphics.getWidth()/2 - sprite.getWidth()/2, Gdx.graphics.getHeight()/2 - sprite.getHeight()/2);
+
 		Music mp3Music = Gdx.audio.newMusic(Gdx.files.internal("Switch-On.mp3"));
 		mp3Music.play();
 		mp3Music.setVolume(1.0f);
@@ -69,44 +69,5 @@ public class GDXgame extends ApplicationAdapter implements InputProcessor{
 		img.dispose();
 
 	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
-	}
 }
+
---
+++ core/src/com/rexserubii/solacedhistoria/MainMenu.java
@@ -0,0 +1,10 @@
+package com.rexserubii.solacedhistoria;
+
+import java.util.AbstractSet;
+
+/**
+ * Created by Rex on 10/11/2016.
+ */
+
+public class MainMenu {
+}
